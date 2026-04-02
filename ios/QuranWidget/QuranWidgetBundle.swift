7366b54cf6b2d530fd80ac451581ed9678dfcf43
Abdullah <connecttopathan@gmail.com>
2026-04-02 20:41:47 +0400

    Add iOS adhan notification .caf sound clips (28s)

---
+++ ios/QuranWidget/QuranWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  QuranWidgetBundle.swift
+//  QuranWidget
+//
+//  Created by Abdullah Khan on 31/03/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct QuranWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        QuranWidget()
+    }
+}
